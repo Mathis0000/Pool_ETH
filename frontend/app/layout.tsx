5f749ad3cc04fa69c64a5a5a467ec43a4df3ee18
mathis <mathisvincent913@gmail.com>
2024-04-30 12:36:37 +0200

    v1 deployment

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,25 +1,22 @@
 'use client'
 import { ChakraProvider } from '@chakra-ui/react'
 import '@rainbow-me/rainbowkit/styles.css';
-
+import { sepolia } from 'wagmi/chains';
 
 import {
   getDefaultWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  hardhat
-} from 'wagmi/chains';//mainnet,  polygon,  optimism,  arbitrum,  base,  zora, // a la place hardat
 
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
-  [hardhat],
+  [sepolia],
   [
     publicProvider()
   ]
-);//mainnet,  polygon,  optimism,  arbitrum,  base,  zora, // a la place hardat
+);
 
 const { connectors } = getDefaultWallets({
   appName: 'Pool App',
@@ -33,15 +30,13 @@ const wagmiConfig = createConfig({
   publicClient
 })
 
-
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body>
         <WagmiConfig config={wagmiConfig}>
           <RainbowKitProvider chains={chains}>
